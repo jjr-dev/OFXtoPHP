299c158fb7126c0e66fb891ad8c1995d999dd651
Patrik da silva Schwindt <68918654+patrikdasilva@users.noreply.github.com>
2021-09-17 12:14:59 -0300

    Add files via upload

--- src/OFXtoPHP.php
+++ src/OFXtoPHP.php
@@ -1,134 +1,138 @@
 <?php
-  class OFXtoPHP
-  {
-      private $file;
-      private $configs;
-      private $ofx;
 
-      function __construct()
-      {
-          $this->configs = (object) array(
-              'closeTags' => true,
-              'returnObject' => true
-          );
-      }
+class OFXtoPHP
+{
+    private $file;
+    private $configs;
+    private $ofx;
 
-      function applyMask($mask, $string)
-      {
-          $mask = str_replace('#', '%s', $mask);
-          return  vsprintf($mask, str_split($string));
-      }
+    function __construct()
+    {
+        $this->configs = (object) array(
+            'closeTags' => true,
+            'returnObject' => true
+        );
+    }
 
-      function setFile($path)
-      {
-          $this->file = file_get_contents($path);
-      }
+    function applyMask($mask, $string)
+    {
+        $mask = str_replace('#', '%s', $mask);
+        return  vsprintf($mask, str_split($string));
+    }
 
-      function setConfigs($configs)
-      {
-          if (isset($configs['closeTags'])) {
-              $this->configs->closeTags = $configs['closeTags'];
-          }
+    function setFile($path)
+    {
+        $this->file = file_get_contents($path);
+    }
 
-          if (isset($configs['returnObject'])) {
-              $this->configs->returnObject = $configs['returnObject'];
-          }
-      }
+    function setConfigs($configs)
+    {
+        if (isset($configs['closeTags'])) {
+            $this->configs->closeTags = $configs['closeTags'];
+        }
 
-      function convertOfxToArray()
-      {
-          $start_ofx  = strstr($this->file, "<OFX>", true);
-          $ofx_str    = str_replace($start_ofx, '', $this->file);
+        if (isset($configs['returnObject'])) {
+            $this->configs->returnObject = $configs['returnObject'];
+        }
+    }
 
-          if ($this->configs->closeTags) {
-              $ofx_exp    = explode("\n", $ofx_str);
+    function convertOfxToArray()
+    {
+        $start_ofx  = strstr($this->file, "<OFX>", true);
+        $ofx_str    = str_replace($start_ofx, '', $this->file);
 
-              $ofx_full   = '';
+        if ($this->configs->closeTags) {
+            $ofx_exp    = explode("\n", $ofx_str);
 
-              foreach ($ofx_exp as $line) {
-                  if (strpos($line, '</') === false) {
+            $ofx_full   = '';
 
-                      $endTag = strpos($line, '>');
+            foreach ($ofx_exp as $line) {
+                $line = trim($line);
+                if (strpos($line, '</') === false) {
 
-                      $lineTag = substr($line, 1, $endTag - 1);
-                      $content = substr($line, $endTag + 1);
+                    $endTag = strpos($line, '>');
 
-                      if (strlen($content) > 0) {
-                          $ofx_full .= "<{$lineTag}>$content</{$lineTag}>\n";
-                      } else {
-                          $ofx_full .=  "<{$lineTag}>\n";
-                      }
-                  } else {
-                      $ofx_full .=  "$line\n";
-                  }
-              }
+                    $lineTag = substr($line, 1, $endTag - 1);
+                    $content = substr($line, $endTag + 1);
 
-              $ofx_full   = trim(str_replace('<>', '', $ofx_full));
-          } else {
-              $ofx_full = $ofx_str;
-          }
+                    if (strlen($content) > 0) {
+                        $ofx_full .= "<{$lineTag}>$content</{$lineTag}>\n";
+                    } else {
+                        $ofx_full .=  "<{$lineTag}>\n";
+                    }
+                } else {
+                    $ofx_full .=  "$line\n";
+                }
+            }
 
-          $ofx_xml    = simplexml_load_string($ofx_full, "SimpleXMLElement", LIBXML_NOCDATA);
-          $ofx_json   = json_encode($ofx_xml);
-          $this->ofx  = json_decode($ofx_json, true);
+            $ofx_full   = trim(str_replace('<>', '', $ofx_full));
+        } else {
+            $ofx_full = $ofx_str;
+        }
 
-          return $this->ofx;
-      }
+        $ofx_xml    = simplexml_load_string($ofx_full, "SimpleXMLElement", LIBXML_NOCDATA);
+        $ofx_json   = json_encode($ofx_xml);
+        $this->ofx  = json_decode($ofx_json, true);
 
-      function getBank()
-      {
-          $return = array(
-              'name'  => $this->ofx['SIGNONMSGSRSV1']['SONRS']['FI']['ORG'],
-              'id'    => $this->ofx['SIGNONMSGSRSV1']['SONRS']['FI']['FID']
-          );
+        return $this->ofx;
+    }
 
-          return ($this->configs->returnObject) ? (object) $return : (array) $return;
-      }
+    function getBank()
+    {
+        if(isset($this->ofx['SIGNONMSGSRSV1']['SONRS']['FI'])) {
+            $return['name'] = $this->ofx['SIGNONMSGSRSV1']['SONRS']['FI']['ORG'];
+        }
 
-      function getDates()
-      {
-          $mask = '####-##-## ##:##:##';
+        $return['id'] = $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKACCTFROM']['BANKID'];
+        
 
-          $return = array(
-              'generate'  =>  $this->applyMask($mask, $this->ofx['SIGNONMSGSRSV1']['SONRS']['DTSERVER']),
-              'start'     =>  $this->applyMask($mask, $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['DTSTART']),
-              'end'       =>  $this->applyMask($mask, $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['DTEND'])
-          );
+        return ($this->configs->returnObject) ? (object) $return : (array) $return;
+    }
 
-          return ($this->configs->returnObject) ? (object) $return : (array) $return;
-      }
+    function getDates()
+    {
+        $mask = '####-##-## ##:##:##';
 
-      function getAccount()
-      {
-          $return = array(
-              'account'  =>  $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKACCTFROM']['ACCTID'],
-              'type'     =>  $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKACCTFROM']['ACCTTYPE']
+        $return = array(
+            'generate'  =>  $this->applyMask($mask, $this->ofx['SIGNONMSGSRSV1']['SONRS']['DTSERVER']),
+            'start'     =>  $this->applyMask($mask, $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['DTSTART']),
+            'end'       =>  $this->applyMask($mask, $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['DTEND'])
+        );
 
-          );
+        return ($this->configs->returnObject) ? (object) $return : (array) $return;
+    }
 
-          return ($this->configs->returnObject) ? (object) $return : (array) $return;
-      }
+    function getAccount()
+    {
+        $return = array(
+            'account'  =>  $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKACCTFROM']['ACCTID'],
+            'type'     =>  $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKACCTFROM']['ACCTTYPE']
 
-      function getTransactions()
-      {
-          $transactions = $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['STMTTRN'];
+        );
 
-          $object = array();
-          foreach($transactions as $transaction) {
-              $transactionOrganized = array(
-                  'id'            => $transaction['FITID'],
-                  'type'          => $transaction['TRNTYPE'],
-                  'description'   => $transaction['MEMO'],
-                  'amount'        => $transaction['TRNAMT'] * -1,
-                  'date'          => $this->applyMask('####-##-## ##:##:##', $transaction['DTPOSTED']),
-                  'positive'      => ($transaction['TRNAMT'] > 0) ? true : false
-              );
+        return ($this->configs->returnObject) ? (object) $return : (array) $return;
+    }
 
-              $transactionOrganized = ($this->configs->returnObject) ? (object) $transactionOrganized : (array) $transactionOrganized;
+    function getTransactions()
+    {
+        $transactions = $this->ofx['BANKMSGSRSV1']['STMTTRNRS']['STMTRS']['BANKTRANLIST']['STMTTRN'];
 
-              $object[$transaction['FITID']] = $transactionOrganized;
-          }
+        $object = array();
+        foreach($transactions as $transaction) {
+            $transactionOrganized = array(
+                'id'            => $transaction['FITID'],
+                'type'          => $transaction['TRNTYPE'],
+                'description'   => $transaction['MEMO'],
+                'amount'        => abs($transaction['TRNAMT']),
+                'date'          => $this->applyMask('####-##-## ##:##:##', $transaction['DTPOSTED']),
+                'positive'      => ($transaction['TRNAMT'] > 0) ? true : false
+            );
 
-          return ($this->configs->returnObject) ? (object) $object : (array) $object;
-      }
-  }
+            $transactionOrganized = ($this->configs->returnObject) ? (object) $transactionOrganized : (array) $transactionOrganized;
+
+            $object[$transaction['FITID']] = $transactionOrganized;
+        }
+
+        return ($this->configs->returnObject) ? (object) $object : (array) $object;
+    }
+}
